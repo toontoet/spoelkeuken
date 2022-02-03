92b18916184897f7ffec7104bb22c36590473e97
Toon Toetenel <toon@breakingbits.nl>
2022-02-03 15:39:03 +0100

    Added Industry selection to Organisations for better organisation

---
+++ spoelkeuken/spoelkeuken/doctype/industry/industry.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, PublicSpaces and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Industry(Document):
+	pass
